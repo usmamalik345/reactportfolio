63d04cd251d30fd8f7c0913a044c0cd7efac5a8a
usmamalik345 <usmamalik344@gmail.com>
2022-07-05 14:29:33 -0700

    new updates

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,7 @@ import airbnd from "../assets/Airbnb.png"
 import desifood from "../assets/desifood.png"
 import foodapp from "../assets/foodapp.png"
 import lms from "../assets/lmsclone.png"
-
+import hoooks from "../assets/hooks.png"
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
@@ -22,7 +22,7 @@ const Work = () => {
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${lms})` }}
+            style={{ backgroundImage: `url(${desifood})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
@@ -116,7 +116,7 @@ const Work = () => {
           </div>
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${hoooks})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
@@ -139,16 +139,16 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${lms})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                DevNaton Clone
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://github.com/usmamalik345/lms-clone'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
